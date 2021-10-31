41a5687c0af833d09a59014fd003fa85db44079a
Gabriel Farrell <gabriel.farrell@gmail.com>
2021-10-31 12:14:32 -0400

    Update tests with the latest dummy data.

--- services_test.go
+++ services_test.go
@@ -21,8 +21,8 @@ func Test_services(t *testing.T) {
 			status, http.StatusOK)
 	}
 
-	// Confirm the response has the expected data?
-	contains := `{ "id": "ccb5fa2c-d6da-4d68-a901-3d81682e3a2c", "name": "o_Service", "description": "o blah, blah, blah",`
+	// Confirm the response contains an expected substring?
+	contains := `{"id":"1","name":"a_Service","description":"a blah, blah, blah","versionCount":1,"url":"https://example.com/a_Service"}`
 	if !strings.Contains(recorder.Body.String(), contains) {
 		t.Errorf("Response does not contains expected content: %v is missing %v",
 			recorder.Body.String(), contains)
@@ -30,8 +30,9 @@ func Test_services(t *testing.T) {
 }
 
 // Acceptance test for /services/{id}
+// Note its checking the service has a URL making navigation to the service possible as requested by Product Owner
 func Test_services_id(t *testing.T) {
-	req, err := http.NewRequest("GET", "/services/ccb5fa2c-d6da-4d68-a901-3d81682e3a2c", nil)
+	req, err := http.NewRequest("GET", "/service_detail/2", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -45,7 +46,8 @@ func Test_services_id(t *testing.T) {
 	}
 
 	// Confirm the response has the expected data?
-	const contains = `{ "id": "ccb5fa2c-d6da-4d68-a901-3d81682e3a2c", "name": "o_Service", "description": "o blah, blah, blah",`
+	//const contains = `{ "id": "ccb5fa2c-d6da-4d68-a901-3d81682e3a2c", "name": "o_Service", "description": "o blah, blah, blah",`
+	const contains = `{"id":"2","name":"b_Service","description":"b blah, blah, blah","versionCount":2,"url":"https://example.com/b_Service"}`
 	if !strings.Contains(recorder.Body.String(), contains) {
 		t.Errorf("Response does not contains expected content: %v is missing %v",
 			recorder.Body.String(), contains)
