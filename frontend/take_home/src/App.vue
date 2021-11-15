7915cbbe55bc89b86e7625a31d38211eea0e8cb0
Gabriel Farrell <gabriel.farrell@gmail.com>
2021-11-15 11:58:53 -0500

    Add all the frontend files added by Vue CLI.

---
+++ frontend/take_home/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <HelloWorld msg="My mess" />
+  <Services />
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Services from './components/Services.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Services
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/take_home/src/components/Services.vue
@@ -0,0 +1,85 @@
+<template>
+    
+
+<div class="ui cards">
+  <div class="card" :v-for="service in services">
+    <div class="content">
+      <div class="header">service.name</div>
+      <div class="description">
+      service.description
+      </div>
+    </div>
+    <div class="ui bottom attached button">
+      <i class="add icon"></i>
+      Add Friend
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Services',
+  props: {
+    msg: String
+  },
+  mounted: function () {
+      window.axios.get('http://localhost:8080/services')
+        .then(response => console.log(response.data))
+        .catch(error => {
+          console.log(error);
+        });
+        //.then(response => this.services = response.data)
+      // read from api    
+      /*
+      window.axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => this.posts = response.data)
+        .catch(error => {
+          console.log(error);
+        });
+        */
+    },
+
+    data: function () {
+      return {
+        services: [ 
+            {
+                "id": "584548",
+                "name": "GABE",
+                "description": "b blah, blah, blah",
+                "versionCount": 222,
+                "url": "https://example.com/b_Service",
+                "versions": null,
+            },
+        ],
+      }
+    },
+
+  methods: {
+    onClick: function () {
+      this.$emit('clicked');
+      console.log('clicked');
+    },
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
